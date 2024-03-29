9d15e27f967f496293d8a7326f32a7122ba61667
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-03-29 16:36:45 +0300

    Ft:models

---
+++ market/models.py
@@ -0,0 +1,20 @@
+from market import db
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password_hash = db.Column(db.String(100), nullable=False)
+    budget=db.Column(db.Integer())
+    items = db.relationship('Item', backref='owner', lazy=True)
+
+class Item(db.Model):
+    __tablename__ = 'items'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(200))
+    price = db.Column(db.Numeric(10, 2), nullable=False)
+    owner_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
