3cc411867a2b6a47fe42d82f0af5bb4f04c19c02
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-03-30 18:10:52 +0300

    Ft:routes

--- market/routes.py
+++ market/routes.py
@@ -2,7 +2,8 @@ from market import app, db
 from flask import render_template, redirect, url_for, flash
 from market.models import Item, User
 from market.forms import RegisterForm
-
+from market.forms import LoginForm
+from flask_login import login_user, logout_user,login_required
 
 
 
@@ -11,6 +12,7 @@ def home_page():
     return render_template('home.html')
 
 @app.route("/market")
+@login_required
 def market_page():
     books = Item.query.all()
     return render_template('market.html', books=books)
@@ -22,9 +24,11 @@ def register_page():
 
         created_user = User(username=form.username.data, 
                              email=form.email_address.data,
-                             password_hash=form.password1.data)
+                             password=form.password1.data)
         db.session.add(created_user)
         db.session.commit()
+        login_user(created_user)
+        flash(f'Account created successfully!.You are now logged in as {created_user.username}',category="success")
         
         flash('Account created successfully!', 'success')
         return redirect(url_for('market_page'))
@@ -32,6 +36,26 @@ def register_page():
         for err_msg in form.errors.values():
             flash(f'there was an error:{err_msg}', category='danger')
     return render_template('register.html', form=form)
+@app.route('/login',methods=['POST', 'GET'])
+def login_page():
+    form=LoginForm()
+    if form.validate_on_submit():
+        attempted_user=User.query.filter_by(username=form.username.data).first()
+        if attempted_user and attempted_user.check_password(
+            attempted_password=form.password.data):
+            login_user(attempted_user)
+            flash(f'You are now logged in:{attempted_user.username}', category='success')
+            return redirect(url_for('market_page'))
+        else:
+            flash('Invalid username or password',category= 'danger')
+    return render_template('login.html', form=form)
+
+@app.route('/logout')
+
+def logout_page():
+    logout_user()
+    flash('logged out successfull!', category='info')
+    return redirect(url_for('home_page'))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True) 
