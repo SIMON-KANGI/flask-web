4321680ce12f3f1360b8beb2cabe22cfc7880077
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-03-30 18:14:00 +0300

    ft:Material

--- market/__init__.py
+++ market/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask,render_template
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 app=Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///market.db'
 app.config['SECRET_KEY']='d5f34bcec7282739912117f6'
 db=SQLAlchemy(app)
-
+bcrypt=Bcrypt(app)
+login_manager=LoginManager(app)
+login_manager.login_view="login_page"
+login_manager.login_message_category="info"
 from market import routes
