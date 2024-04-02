34293ac5f2a9459a0ac00c931d42180916f3d51e
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-04-02 14:55:17 +0300

    ft:init file

--- market/__init__.py
+++ market/__init__.py
@@ -1,11 +1,16 @@
+
+#creates a package called market
+
 from flask import Flask,render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+
 app=Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///market.db'
-app.config['SECRET_KEY']='d5f34bcec7282739912117f6'
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///market.db' # attach the app to sqlalchemy
+app.config['SECRET_KEY']='d5f34bcec7282739912117f6' #generate a secret key
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db=SQLAlchemy(app)
 bcrypt=Bcrypt(app)
 login_manager=LoginManager(app)
