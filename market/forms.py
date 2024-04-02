2a32f864f2674febfc5745c96f561a69441907f2
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-04-02 14:54:20 +0300

    ft:forms

--- market/forms.py
+++ market/forms.py
@@ -1,14 +1,17 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField                            #imports
 from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
 from market.models import User
 
+
+#registartion form
 class RegisterForm(FlaskForm):
+    #method to validate the username
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user:
             raise ValidationError("Username already taken")
-        
+    #method to validate the email address
     def validate_email_address(self, email_address):
         email = User.query.filter_by(email=email_address.data).first()
         if email:
@@ -19,8 +22,22 @@ class RegisterForm(FlaskForm):
     password1 = PasswordField(label="Password", validators=[Length(min=6, max=10), DataRequired()])
     password2 = PasswordField(label="Confirm Password", validators=[EqualTo("password1"), DataRequired()])
     submit = SubmitField(label="Register")
-
+#login form
 class LoginForm(FlaskForm):
     username = StringField(label="Username", validators=[DataRequired()])
     password=PasswordField(label="Password", validators=[DataRequired()])
     submit = SubmitField(label="Login")
+#add items form
+class AddItems(FlaskForm):
+    name=StringField(label="Name", validators=[DataRequired()])
+    barcode=StringField(label="Barcode", validators=[Length(min=10, max=10),DataRequired()])
+    description=StringField(label="Description", validators=[Length(min=30, max=1024),DataRequired()])
+    price=  StringField(label="Price", validators=[DataRequired()])
+    img_url=StringField(label="Image URL", validators=[DataRequired()])
+    submit = SubmitField(label="Submit")
+ #purchase form   
+class PurchaseForm(FlaskForm):
+      submit = SubmitField(label="Purchase")
+  #sell form    
+class SellForm(FlaskForm):
+      submit = SubmitField(label="Sell item")
